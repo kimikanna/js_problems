38f442d13a4a761707fe57edb2c92379157a19a9
kimikanna <svartirose@gmail.com>
2025-01-15 16:34:30 +0300

    Solved 19

--- 019-search-anagrams.js
+++ 019-search-anagrams.js
@@ -12,7 +12,29 @@
  * @returns {string}
  */
 function searchAnagrams(value) {
-    return undefined;
+    // Разбиваем строку на слова, исключая знаки препинания
+    const words = value.split(/\s+/).map(word => word.replace(/[.,!?]/g, ''));
+
+    const seen = new Map(); // Хранит количество слов в канонической форме
+
+    // Считаем количество вхождений каждой канонической формы
+    for (const word of words) {
+        const canonicalForm = word.toLowerCase().split('').sort().join('');
+        seen.set(canonicalForm, (seen.get(canonicalForm) || 0) + 1);
+    }
+
+    const result = [];
+
+    // Добавляем только те слова, у которых есть повторяющиеся анаграммы
+    for (const word of words) {
+        const canonicalForm = word.toLowerCase().split('').sort().join('');
+        if (seen.get(canonicalForm) > 1) {
+            result.push(word);
+        }
+    }
+
+    // Собираем результат в строку и возвращаем
+    return result.join(' ');
 }
 
 module.exports = searchAnagrams;
